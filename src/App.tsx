825045bd5b8ef30db510eddc1250388775d3318d
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2022-05-03 01:42:12 -0300

    Feature: ThemeProvider included at the top of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
-const App = () => <h1>App</h1>;
+import { ThemeProvider } from "styled-components";
+import themes from "./shared/themes";
+
+const App = () => (
+    <ThemeProvider theme={themes}>
+        <h1>App</h1>
+    </ThemeProvider>
+);
 
 export default App;
---
+++ src/shared/themes/index.ts
@@ -0,0 +1,8 @@
+const themes = {
+    blue: "blue",
+    red: "red",
+    lightGrey: "#CDCDCD",
+    darkGrey: "#888",
+};
+
+export default themes;
