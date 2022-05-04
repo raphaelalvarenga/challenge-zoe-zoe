96fc7b6b92091ae71cb61e31b145db3b7e8db478
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2022-05-03 23:46:02 -0300

    Feature: Created cards section styles for desktops.

--- src/pages/Assets/index.tsx
+++ src/pages/Assets/index.tsx
@@ -4,27 +4,34 @@ import Card from "../../shared/components/Card";
 import { useEffect, useState } from "react";
 import dataStream from "../../data";
 import { IAssetUpdated } from "../../shared/interfaces/AssetUpdated.interface";
-import Select, { SingleValue } from 'react-select'
+import Select, { SingleValue } from "react-select";
+import { concatAll, map, mergeAll, timer, toArray, zip } from "rxjs";
 
 const Assets = () => {
     const [inputValue, setInputValue] = useState("");
-    const [selectValue, setSelectValue] = useState<SingleValue<{value: string, label: string}>>({value: "all", label: "All"});
-
-    const [assets, setAssets] = useState<IAssetUpdated>({
-        id: 1,
-        assetName: "BABKJ",
-        lastUpdate: Date.now(),
-        previewPrice: 28.2342,
-        price: 28.2341,
-        type: "Stock",
-        market: "Nasdaq",
+    const [selectValue, setSelectValue] = useState<SingleValue<{ value: string; label: string }>>({
+        value: "all",
+        label: "All",
     });
-    const [previewAssets, setPreviewAssets] = useState([]);
+
+    const [assets, setAssets] = useState<IAssetUpdated[]>([]);
+
+    const [filteredAssets, setFilteredAssets] = useState<IAssetUpdated[]>([]);
 
     useEffect(() => {
-        // dataStream.subscribe(
+        // dataStream.subscribe(res => assetsTest.push(res));
+        // zip(dataStream, timer(0, 1000))
+        //     .pipe(map(([delayedInput, _timer]) => delayedInput))
+        //     .subscribe(
+        //         result => {
+        //             setAssets([...assets, { ...result, previewPrice: result.price }]);
+        //         },
+        //         error => console.log(error)
+        //     );
+        // dataStream.pipe(toArray()).subscribe(
         //     result => {
-        //         // assetsTest.push(result);
+        //         console.log("teste");
+        //         console.log(result);
         //         // setAssets(prev => [...prev, { ...result, previewPrice: result.price }]);
         //     },
         //     error => console.log(error)
@@ -33,7 +40,7 @@ const Assets = () => {
 
     useEffect(() => {
         if (selectValue?.value === "all") {
-            setFilteredAssets(assets)
+            setFilteredAssets(assets);
             return;
         }
 
@@ -44,17 +51,24 @@ const Assets = () => {
         <Container>
             <FiltersContainer>
                 <Input inputValue={inputValue} setInputValue={setInputValue} />
-            <Select
-                value={selectValue}
-                name="assetType"
-                options={[{value: "all", label: "All"}, {value: "stock", label: "Stock"}, {value: "currency", label: "Currency"}]}
-                onChange = {(newValue: SingleValue<{value: string, label: string}>) => setSelectValue(newValue)}
-            />
+                <Select
+                    value={selectValue}
+                    name="assetType"
+                    options={[
+                        { value: "all", label: "All" },
+                        { value: "stock", label: "Stock" },
+                        { value: "currency", label: "Currency" },
+                    ]}
+                    onChange={(newValue: SingleValue<{ value: string; label: string }>) => setSelectValue(newValue)}
+                />
             </FiltersContainer>
 
-
             <CardsContainer>
-                {filteredAssets.filter(asset => asset.assetName.includes(inputValue.toUpperCase())).map(asset => <Card key={asset.id} asset={asset} />)}
+                {filteredAssets
+                    .filter(asset => asset.assetName.includes(inputValue.toUpperCase()))
+                    .map(asset => (
+                        <Card key={asset.id} asset={asset} />
+                    ))}
             </CardsContainer>
         </Container>
     );
--- src/pages/Assets/style/index.ts
+++ src/pages/Assets/style/index.ts
@@ -6,6 +6,12 @@ export const Container = styled.div`
 
 export const FiltersContainer = styled.div`
     margin: 20px 0 40px 0;
+    display: grid;
+    grid-template-columns: auto auto;
 `;
 
-export const CardsContainer = styled.div``;
+export const CardsContainer = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fill, 400px);
+gap: 50px 80px;
+`;
